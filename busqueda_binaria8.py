ecbbe919b2132bce9322721eb60e70bab5f3bbc4
Cesar Hera <cesar.hr2003@gmail.com>
2021-01-29 16:52:21 -0600

    Agrego programa de busqueda binaria e implemento time en busqueda lineal y binaria

--- busqueda_binaria8.py
+++ busqueda_binaria8.py
@@ -1,26 +1,32 @@
 import random
+import time
+
 
 def busqueda_binaria(objetivo, comienzo, final, lista):
     print(f'{objetivo}, {lista[comienzo]}, {lista[final]}')
-    if comienzo == final:
-        return False
     
     medio = (comienzo + final) // 2
 
     if lista[medio] == objetivo:
         return True
+    elif comienzo > final:
+        return False
     elif lista[medio] < objetivo:
         return busqueda_binaria(objetivo, medio + 1, final, lista)
     else:
         return busqueda_binaria(objetivo, comienzo, medio - 1, lista)
 
 
+
 if __name__ == '__main__':
+    
     objetivo = int(input('¿Cuál es el número a encontrar?: '))
     lista_tamaño = int(input('¿De qué tamaño será la lista?: '))
     lista = sorted([random.randint(0, 100) for i in range(lista_tamaño)])
 
+    start = time.time()
     nice = busqueda_binaria(objetivo, 0, len(lista) - 1, lista)
-
     print(lista)
     print(f'{objetivo} {"Está en la lista" if nice else "No está en la lista"}')
+    end = time.time()
+    print(end - start)
--- busqueda_lineal7.py
+++ busqueda_lineal7.py
@@ -1,21 +1,31 @@
 import random
+import time
+
 
 def busqueda_lineal(objetivo, lista): # Complejidad algorítmica: O(n)
     
+    contador = 0
+
     for i in lista:
+        contador += 1
         if i == objetivo:
-            return True
+            return [contador, True]
     
-    return False
+    return [contador, False]
+
 
 
 if __name__ == '__main__':
+
     objetivo = int(input('¿Ingresa el número que quieres encontrar?: '))
     lista_tamaño = int(input('¿De qué tamaño genero la lista aleatoria?: '))
+    lista = [random.randint(1, 100) for i in range(lista_tamaño)]
 
-    lista = sorted([random.randint(1, 100) for i in range(lista_tamaño)])
+    start = time.time()
+    contador, nice = busqueda_lineal(objetivo, lista)
 
-    nice = busqueda_lineal(objetivo, lista)
-
-    print(lista)
+    # print(lista)
     print(f'{objetivo} {"está en la lista" if nice else "no está en la lista"}')
+    end = time.time()
+    print((end - start))
+    print(contador)
